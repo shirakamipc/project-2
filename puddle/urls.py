30768a6cd77e55a69206f1e75ca877da541da7b5
shirakamipc <duongnhatthanh2001@gmail.com>
2023-05-23 13:30:57 +0700

    update

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for marketplace project.
+URL configuration for puddle project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
